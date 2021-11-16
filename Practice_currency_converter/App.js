ed214a54d821ec68af1c05a277ec84570580aeb6
ajitkumar2804 <ajitjagtap67@gmail.com>
2021-11-16 10:22:21 +0530

    solved currency converter problem

--- Practice_currency_converter/App.js
+++ Practice_currency_converter/App.js
@@ -1,2 +1,19 @@
-const usd =  document.querySelector("#usr")
-const inr = document.querySelector("#inr")
+const usd =  document.querySelector("#usr");
+const inr = document.querySelector("#inr");
+const btncon =  document.querySelector("#convert");
+
+
+btncon.addEventListener("click", clickHandler)
+
+
+function clickHandler(){
+
+    let value = Number(usd.value)
+    fetch("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json")
+    .then(response=> response.json())
+    .then(data =>{
+        console.log(data.inr)
+        let ct = data.usd.inr;
+        inr.innerText = ct*value;
+    })
+}
